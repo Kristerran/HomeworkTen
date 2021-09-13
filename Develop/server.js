1d3f4d4fb8c017ad56bdb5e7fef61c84014c5155
kristerran <kristopherbaily@gmail.com>
2021-09-12 20:38:13 -0700

    Having issues with saving new note to file, commit for troubleshooting

--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
 const PORT = 3001;
+const fs = require('fs')
+const notes = require('./db/db.json')
 
+app.use(express.json())
 app.use(express.static('public'))
 app.get('/', (req, res) => 
   res.sendFile(path.join(__dirname, '/public/index.html'))
@@ -13,13 +15,30 @@ app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// get request for notes
 app.get('/api/notes', (req, res) => {
-  readFromFile('./db/db.json').then(data => JSON.parse(data))
+  console.info(`GET /api/notes`);
+  res.status(200).json(notes);
 });
-
-// app.post('/notes', (req, res) =>
-  
-// );
+// POST request to add a note
+app.post('/api/notes', (req, res) => {
+  // Log that a POST request was received
+  console.info(`${req.method} request received to add a note`);
+  let newNote = req.body
+  notes.push(
+    newNote
+  )
+  let noteString = JSON.stringify(notes)
+  fs.appendFile('./db/db.json', noteString, (err) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      // Get the file contents after the append operation
+      console.log('note added successfully')
+    }
+  });
+});
 
 app.listen(PORT, () =>
   console.log(`Example app listening at http://localhost:${PORT}`)
