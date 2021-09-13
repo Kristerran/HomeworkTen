1a1ceea6a5bf76cb54dc4963999919c750631f1e
kristerran <kristopherbaily@gmail.com>
2021-09-13 13:48:41 -0700

    Fixed post line (was missing res)

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const path = require('path');
 const app = express();
 var PORT = process.env.PORT || 3001;
 const fs = require('fs')
-const notes = require('./db/db.json')
 
 app.use(express.json())
 app.use(express.static('public'))
@@ -18,23 +17,29 @@ app.get('/notes', (req, res) =>
 // get request for notes
 app.get('/api/notes', (req, res) => {
   console.info(`GET /api/notes`);
-  res.status(200).json(notes);
+  let notes = fs.readFileSync('./db/db.json')
+  let notesJson = JSON.parse(notes)
+  res.status(200).json(notesJson);
 });
 // POST request to add a note
 app.post('/api/notes', (req, res) => {
   // Log that a POST request was received
   console.info(`${req.method} request received to add a note`);
   let newNote = req.body
-  notes.push(
+  let notes = fs.readFileSync('./db/db.json')
+  let notesJson = JSON.parse(notes)
+  notesJson.push(
     newNote
   )
-  let noteString = JSON.stringify(notes)
+  let noteString = JSON.stringify(notesJson)
   fs.writeFile('./db/db.json', noteString, (err) => {
     if (err) {
       console.log(err);
+      res.json('ERR')
     }
     else {
       console.log('note added successfully')
+      res.json('SUCCESS')
     }
   });
 });
